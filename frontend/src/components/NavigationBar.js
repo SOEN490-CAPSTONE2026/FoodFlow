114e47470b8e0795218a506ba55c5cd947cfea46
Hawa Said <hawa21244@gmail.com>
2025-09-26 00:45:29 -0400

    #16 Added the logo in the navbar and linked it to our Homepage

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
+import { Link } from "react-router-dom";
+import Logo from "../assets/Logo.png";
 
 const NavigationBar = () => {
   const navigate = useNavigate();
@@ -18,8 +20,9 @@ const NavigationBar = () => {
   return (
     <nav style={{ padding: '1rem', background: '#f8f9fa', display: 'flex', justifyContent: 'space-between' }}>
       <div>
-        <span style={{ fontSize: '1.5rem' }}>FoodFlow</span>
-      </div>
+        <Link to="/">
+          <img src={Logo} alt="Logo" height={50} width={50} />
+        </Link>      </div>
       <div>
         {!isLoggedIn && (
           <button onClick={handleLogin} style={{ fontSize: '1rem', padding: '0.5rem 1rem', marginRight: '0.5rem' }}>
